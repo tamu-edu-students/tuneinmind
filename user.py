8a79456df2fbd1d09d6344b50926c0176ee58cb7
sahithiakiri <sahithiakiritamu@gmail.com>
2023-04-29 17:10:05 -0500

    templates

---
+++ user.py
@@ -0,0 +1,17 @@
+class User:
+    def _init_(self):
+        self.name = None
+        self.session_id = None
+    def selectSongFromPopularSongs():
+        '''Select a song to play from the list of Popular Songs'''
+        pass
+    def deleteSongFromCurrentSession():
+        pass
+    def enableLyrics():
+        pass
+    def disableLyrics():
+        pass
+    def startSession():
+        pass
+    def endSession():
+        pass
